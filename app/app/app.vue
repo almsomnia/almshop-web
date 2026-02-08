87dbb4e288d1f11bf24c51503dd0964fbfaa8ddc
Riva Almero <rvalms@icloud.com>
2026-02-08 20:12:16 +0700

    feat: Initialize Nuxt project with pnpm workspaces, layered architecture, and development tooling.

---
+++ app/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+   <div>
+      <NuxtRouteAnnouncer />
+      <!-- <NuxtWelcome /> -->
+       <div class="bg-red-500">
+         lorem
+       </div>
+   </div>
+</template>
---
+++ layers/core/ui/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+   compatibilityDate: "2025-07-15",
+   devtools: { enabled: false },
+   modules: ["@nuxt/ui"],
+   css: [join(currentDir, "app/assets/css/main.css")],
+   components: {
+      dirs: [
+         {
+            path: join(currentDir, "app/components"),
+            pathPrefix: false,
+         }
+      ]
+   }
+})
