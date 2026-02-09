e8e0fac1145152e8b4514a2cb7edff1bf1f7e53c
Riva Almero <rivaalms@proton.me>
2026-02-09 12:05:58 +0700

    refactor: specify login API response type as DTO.Auth

--- layers/domains/auth/server/api/login.post.ts
+++ layers/domains/auth/server/api/login.post.ts
@@ -2,7 +2,7 @@ export default eventHandler(async (event) => {
    const $api = $serverApi(event)
    const body = await readBody(event)
 
-   return await $api<API.Response<any>>(`/auth/login`, {
+   return await $api<API.Response<DTO.Auth>>(`/auth/login`, {
       method: "post",
       body
    })
