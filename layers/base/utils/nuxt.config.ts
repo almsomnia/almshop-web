95a875e54c369996c976fe3137035d798e331d38
Riva Almero <rivaalms@proton.me>
2026-02-09 12:35:52 +0700

    feat: Add Pinia, VueUse, and Day.js modules with their respective configurations and dependencies.

--- layers/base/utils/nuxt.config.ts
+++ layers/base/utils/nuxt.config.ts
@@ -7,6 +7,14 @@ export default defineNuxtConfig({
    compatibilityDate: "2025-07-15",
    devtools: { enabled: true },
    extends: [],
+   modules: ["@vueuse/nuxt", "dayjs-nuxt", "@pinia/nuxt"],
+
+   dayjs: {
+      plugins: ["utc", "relativeTime", "timezone"],
+      locales: ["id"],
+      defaultLocale: "id",
+      defaultTimezone: "Asia/Jakarta",
+   },
 
    // all paths are relative from ./.nuxt
    typescript: {
