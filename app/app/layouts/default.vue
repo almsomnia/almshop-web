4895aba3e32ee706c3cc0b97c4bbd5e02de6dceb
Riva Almero <rivaalms@proton.me>
2026-02-15 13:48:00 +0700

    feat: add a footer with newsletter signup and dynamic copyright year.

--- app/app/layouts/default.vue
+++ app/app/layouts/default.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const currentDate = Date.now()
+</script>
 
 <template>
    <div>
@@ -6,5 +8,52 @@
       <UMain>
          <slot />
       </UMain>
+      <USeparator />
+      <UFooter
+         :ui="{
+            container: 'max-w-full',
+         }"
+      >
+         <template #top>
+            <UContainer class="max-w-full">
+               <div class="flex justify-between">
+                  <div>
+                     <h2 class="text-3xl font-bold">Join Our Newsletter</h2>
+                     <p class="text-dimmed dark:text-muted mt-1 text-lg">
+                        Subscribe to get special offers, free giveaways, and
+                        once-in-a-lifetime deals.
+                     </p>
+                  </div>
+                  <div class="self-end-safe">
+                     <UForm>
+                        <UFieldGroup>
+                           <UInput
+                              placeholder="Enter your email"
+                              size="lg"
+                              color="neutral"
+                              class="w-xs"
+                           />
+                           <UButton
+                              label="Subscribe"
+                              size="lg"
+                              color="neutral"
+                           />
+                        </UFieldGroup>
+                     </UForm>
+                  </div>
+               </div>
+            </UContainer>
+         </template>
+         <template #left>
+            <p class="text-muted text-sm">
+               &copy;
+               <NuxtTime
+                  :datetime="currentDate"
+                  year="numeric"
+               />
+               Almshop
+            </p>
+         </template>
+      </UFooter>
    </div>
 </template>
